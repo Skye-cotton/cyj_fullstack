534f332f36c1fbf835d3b4ab079048717be835a9
xxxxxxxxxxxxl <362535698@qq.com>
2021-01-29 20:11:58 +0800

    git command

---
+++ react/antd/antd-demo/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+//引入antd
+//import { Button } from 'antd'  //组件库
+import Login from './Login'
+import Post from './Post'// 新的页面组件
+
+function App() {
+  return (
+    <div className="App">
+      <Post />
+      {/* <Login /> */}
+      {/* <Button type="primary" >primary Button</Button> */}
+    </div>
+  );
+}
+
+export default App;
