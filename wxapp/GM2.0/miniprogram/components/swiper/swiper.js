b52867b69311f598092c7ad22753a6876bfcb2ae
xxxxxxxxxxxxl <362535698@qq.com>
2021-02-01 17:53:29 +0800

    git command

---
+++ wxapp/GM2.0/miniprogram/components/swiper/swiper.js
@@ -0,0 +1,69 @@
+// miniprogram/components/swiper/swiper.js
+Page({
+  properties: {
+    imgUrls: Object
+  },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls:['http://img13.360buyimg.com/cms/jfs/t29680/64/1430739322/190412/ef426e1d/5ce03c23Nf867d31e.jpg!q80.webp','http://img10.360buyimg.com/cms/jfs/t1/70971/38/68/200181/5ce03c94Ec6a32c91/2dc0bad2377970bb.png!q80.webp','http://img13.360buyimg.com/cms/jfs/t30679/135/1453639293/202148/93f2976f/5ce03cc2N44174369.jpg!q80.webp']
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
