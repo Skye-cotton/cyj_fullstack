c4dd4693017f31da6a3a440834cbc59a0171c064
xxxxxxxxxxxxl <362535698@qq.com>
2021-02-01 20:43:14 +0800

    git command

---
+++ js/deep_clone/2.js
@@ -0,0 +1,17 @@
+function simpleClone(initialObj){
+    var obj ={}
+    for(var key in initialObj){
+        obj[key] = initialObj[key]
+    }
+    return obj
+}
+
+var obj = {
+    a:"hello",
+    c:["Bob","Tom","Jenny"]
+}
+
+
+var cloneObj = simpleClone(obj)
+cloneObj.a="你好"
+console.log(obj.a);
