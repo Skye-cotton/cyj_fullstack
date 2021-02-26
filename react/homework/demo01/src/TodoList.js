304f93074be3e65dd98bab94563a334bf12f7601
xxxxxxxxxxxxl <362535698@qq.com>
2021-02-27 00:06:00 +0800

    git command

--- react/homework/demo01/src/TodoList.js
+++ react/homework/demo01/src/TodoList.js
@@ -1,10 +1,33 @@
 import React,{ Component } from 'react';
+import 'antd/dist/antd.css'
+import { Input , Button ,List} from 'antd'
+import store from './store'
+
 
 class TodoList extends Component{
 
+    constructor(props){
+        super(props)
+        console.log(store.getState());
+        this.state=store.getState()
+    }
     render(){
         return(
-            <div>Hello world</div>
+            <div style={{margin:'10px'}}>
+                <div>
+                    <Input
+                     placeholder={this.state.inputValue}
+                     style={{width:'250px',marginRight:'10px'}}></Input>
+                     <Button type="primary">增加</Button>
+                </div>
+                <div style={{margin:'10px',width:'300px'}}>
+                    <List
+                        bordered
+                        dataSource={this.state.list}
+                        renderItem={item=>(<List.Item>{item}</List.Item>)}
+                    />
+                </div>
+            </div>
         );
     }
 }
---
+++ react/homework/demo01/src/store/reducer.js
@@ -0,0 +1,11 @@
+const defaultState={
+    inputValue:'Write Something',
+    list:[
+        '考试全都过',
+        '今年六月过四级',
+        '找到实习工作'
+    ]
+}
+export default (state=defaultState,action)=>{
+    return state
+}
