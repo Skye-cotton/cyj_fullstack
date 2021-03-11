5705ec00de2ef8df48358107753b5951734acfdf
xxxxxxxxxxxxl <362535698@qq.com>
2021-03-11 20:58:32 +0800

    git command

--- Interview notes/Valid bracket/3.js
+++ Interview notes/Valid bracket/3.js
@@ -8,10 +8,31 @@ function longestValidParentheses(s){
     // 每位括号它的有效括号匹配长度是可以跟之前的有效匹配是一样的、
     for(var i=0;i<s.length;i++){
         var tmpMax =0;//当前位括号的匹配长度  重新开始
-        for(var j=i;j<s.length;j++){
-            
+        for(var j=i;j<s.length;j++){//从i开始，自己也算+1
+            if(s[j]=='('){//左括号
+                stack.push('(') //入栈
+                tmpMax++;
+            }else if(s[j] == ')'){
+                // 右括号
+                if(stack.length <1){
+                    //栈空的，当前位置括号有效匹配，结束了，算
+                    max =max<tmpMax?tmpMax:max;
+                    break;
+                }else{
+                    stack.pop() //出栈
+                    tmpMax++;
+                }
+                // stack.pop() //出栈
+                // tmpMax++;
+            }    
         }
-    }
+    
+    if(stack.length ==0){//都匹配 从当前位置到最后一个字符都是有效括号
+        max = max <tmpMax?tmpMax:max
 
+    }
+    stack=[] //清空本次括号的服务
+}
     return max;
 }
+console.log(longestValidParentheses(')))))'));
